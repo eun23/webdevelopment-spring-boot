eb63a7a3cc20b4855a667eef014a8c84d4dc8384
eun23 <127043779+eun23@users.noreply.github.com>
2024-08-14 18:02:44 +0900

    2024.08.14

---
+++ ex11_validation_initbinder/src/main/java/com/example/ex11_validation_initbinder/ex11_validation_initbinder.java
@@ -0,0 +1,13 @@
+package com.example.ex11_validation_initbinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ex11_validation_initbinder {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ex11_validation_initbinder.class, args);
+	}
+
+}
