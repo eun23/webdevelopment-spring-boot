3dccbe13b7a44006b42305882f851073692cfb3c
eun23 <127043779+eun23@users.noreply.github.com>
2024-08-20 01:08:58 +0900

    2024.08.20

---
+++ ex20_service/src/main/java/com/example/ex20_service/Ex20MybatisResultNumApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex20_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex20MybatisResultNumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex20MybatisResultNumApplication.class, args);
+	}
+
+}
