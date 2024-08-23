af562438e6290824be4319f82d83cb0ea3a9026c
eun23 <127043779+eun23@users.noreply.github.com>
2024-08-23 17:03:38 +0900

    2024.08.23

---
+++ ex24_transaction_propagation/src/main/java/com/example/ex24_transaction_propagation/ex24_transaction_propagation.java
@@ -0,0 +1,13 @@
+package com.example.ex24_transaction_propagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ex24_transaction_propagation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ex24_transaction_propagation.class, args);
+	}
+
+}
