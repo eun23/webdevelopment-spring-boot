66975dc9e15c05611ab8888983af36b29ae1c869
eun23 <127043779+eun23@users.noreply.github.com>
2024-08-26 16:38:18 +0900

    2024.08.26

---
+++ ex29_jpa_board_rest_api/src/main/java/com/example/ex29_jpa_board_rest_api/Ex29JpaBoardRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex29_jpa_board_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex29JpaBoardRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex29JpaBoardRestApplication.class, args);
+	}
+
+}
